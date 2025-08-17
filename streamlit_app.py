fb85a6281824b8169d2b874e6f8b8fb4f38e4fbd
AB <92795226+ayyannab@users.noreply.github.com>
2025-08-17 17:45:42 +0530

    streamlit update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,10 @@ st.set_page_config(layout="wide")
 
 st.markdown(
     """
-    <h2 style="color:white; text-align:center;">
+    <h3 style="color:white; text-align:center;">
         BAC's Speed Test by Ookla
-    </h2>
-    <iframe src="https://bacn.speedtestcustom.com/" width="100%" height="800px" style="border:none;"></iframe>
+    </h3>
+    <iframe src="https://bacn.speedtestcustom.com/" width="100%" height="1000px" style="border:none;"></iframe>
     <h4 style="color:white; text-align:center;">©️ BAC speedtest</h4>
     """,
     unsafe_allow_html=True
